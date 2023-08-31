ad1078b1171eae7f8961a4b5afd95165fa05a03a
julian corredor <corredor.jul@hotmail.com>
2023-08-31 02:15:39 +0000

    V7

--- Construccion de Proyectos/Practica_php/controller/personas_controller.php
+++ Construccion de Proyectos/Practica_php/controller/personas_controller.php
@@ -4,12 +4,11 @@
 El controlador es la entidad responsable de:
 
 1. llamar el modelo (el espacio donde)
-gestiona solo consulas a bases de datos) 
-*/
+gestiona solo consulas a bases de datos) */
 
 require_once("model/personas_model.php");
-$per=new personas_model();
-$datos=$per->get_personas();
+$per=new personas_model(); // se crea el objeto
+$datos=$per->get_personas(); //instancia el metodo
 
 //2. comunicarse con la vista 
 require_once("view/personas_view.phtml");
